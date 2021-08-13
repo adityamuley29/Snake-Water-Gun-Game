08fd261dc41922960243fdec4638c70119376c29
Aditya muley <61872329+adityamuley29@users.noreply.github.com>
2021-08-13 17:23:00 +0530

    main file added

---
+++ snake_water_gun.py
@@ -0,0 +1,40 @@
+import random
+
+def gameWin(comp, you):
+    if comp == you:
+        return None
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you =='g':
+            return True
+    elif comp == 'w':
+        if you == 's':
+            return True
+        elif you == 'g':
+            return False
+    elif comp == 'g':
+        if you == 's':
+            return  False
+        elif comp == 'w':
+            return  True
+
+print("Computer Turn : Snake(s),Water(w),Gun(g): ")
+randNo = random.randint(1,3)
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'w'
+elif randNo ==3:
+    comp = 'g'
+
+you=input("Your Turn : Snake(s),Water(w),Gun(g): ")
+a = gameWin(comp,you)
+print(f"Computer has chossen {comp}")
+print(f"You has chossen {you}")
+if a == None:
+    print("********* There is a Tie *********")
+elif a:
+    print("******** You Win ***********")
+else:
+    print("******** You Lose *********")
